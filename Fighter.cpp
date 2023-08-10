4ecfbe2f82a090b80e1fc48e438ed670eb0f4fcb
Muhammad Ayman <s-mo_ayman@zewailcity.edu.eg>
2023-08-10 03:17:48 +0300

    riham pending edits

--- Fighter.cpp
+++ Fighter.cpp
@@ -3,6 +3,7 @@
 
 Fighter::Fighter(int id, int arr, double h, double p, double rel, double spd)
     : Enemy(id, 0, arr, h, p, rel, spd)
+
 {
 };
 
@@ -17,5 +18,22 @@ void Fighter::Move()
 
 void Fighter::Act()
 {
+	if (!e->Getreload_period())
+	{
+		if (e->GetTYPE() == FIGHTER)
+		{
+			int k;
+			double DFC = (k / ((e->GetDistance()) * (e->Getpower())));
+			if (e->GetChanged_Health < (0.5 * (e->GetHealth())))
+			{
+				k = 0.5;
+			}
+			else
 
-}
+			{
+				k = 1;
+			}
+			BCastle.SetTotal_damage(BCastle.GetTotal_damage += DFC);
+			e->setfirstshot(CurrentTimeStep);
+		}
+	}
